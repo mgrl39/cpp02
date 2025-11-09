11f8836623459956c1d22a8874025fd74d120cbe
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-11-09 18:19:04 +0100

    add part of fiexdhpp and fxed.cpp

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,8 +6,26 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 23:37:04 by meghribe          #+#    #+#             */
-/*   Updated: 2025/11/07 23:37:04 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/11/08 17:03:33 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
 
+Fixed::Fixed() {}
+
+Fixed::~Fixed() {}
+
+Fixed::Fixed(const Fixed& const) 
+{
+}
+
+// TODO
+int	Fixed::getRawBits(void) const
+{
+	return (0);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+}
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,8 +6,27 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 23:36:58 by meghribe          #+#    #+#             */
-/*   Updated: 2025/11/07 23:36:58 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/11/08 17:07:14 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
 
+class Fixed
+{
+		private:
+				int					fixed_point_nbr_value;
+				static const int	number_fract_bits = 8;
+
+		public:
+				Fixed();
+				Fixed(const Fixed& const);
+				Fixed
+				~Fixed();
+				int		getRawBits(void) const;
+				void	setRawBits(int const raw);
+
+};
+
+#endif
